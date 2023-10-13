b2017a1c676f8526767bfed08d6b65e173bbdaa2
Edo Riansyah <edoriansyah@gmail.com>
2023-10-13 10:04:43 +0700

    class Person

---
+++ Person.php
@@ -0,0 +1,12 @@
+class Person {
+public $nama;
+public $alamat;
+public $jurusan;
+
+public function __construct($nama, $alamat, $jurusan)
+{
+$this->nama = $nama;
+$this->alamat = $alamat;
+$this->jurusan = $jurusan;
+}
+}
