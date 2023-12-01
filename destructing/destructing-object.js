8cf66184acff96ac2e3bd4a6722700f18d48dc26
Edo Riansyah <edoriansyah@gmail.com>
2023-12-01 10:57:07 +0700

    Add destructuring examples for arrays and objects

---
+++ destructing/destructing-object.js
@@ -0,0 +1,12 @@
+const user = {
+    name: "Edo Riansyah",
+    age: 33,
+    address: "Depok",
+    isMarried: true,
+};
+  
+const { name, age, address } = user;
+  
+console.log(name);
+console.log(age);
+console.log(address);
